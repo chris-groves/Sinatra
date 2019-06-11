aa29ab06b89041f5508a2705a73bb24cbacb555e
Chris Groves <chris_groves87@hotmail.co.uk>
2019-06-11 17:28:20 +0100

    using Sinatra forms

--- app.rb
+++ app.rb
@@ -21,6 +21,13 @@ get '/hi!' do
   "Hi"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  puts params
+  @name = params[:name]
   erb(:index)
 end
